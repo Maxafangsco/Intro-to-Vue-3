f85fc5aab376e847cac37389985af75f6d4df8d6
maxafangsco <maxafangsco@gmail.com>
2021-07-07 08:13:20 +0400

    l3-start

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 const app = Vue.createApp({
     data() {
         return {
-            product: 'Socks'
+            product: 'Socks',
+            image: './assets/images/socks_green.jpg',
+            alink: 'https://www.youtube.com/watch?v=vP2MNhC_Igw',
         }
     }
 })
